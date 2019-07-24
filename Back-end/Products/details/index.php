2841e4fddcc4cb83289147f3f189d2d8f9f524fd
plutonia36 <53211532+plutonia36@users.noreply.github.com>
2019-07-24 15:40:29 +0200

    update details.php

--- Back-end/Products/details/index.php
+++ Back-end/Products/details/index.php
@@ -21,7 +21,7 @@ $response = [
 {  
     $id_product = $_REQUEST['id_product'];
 
-    $sql = "SELECT `id_product`,`quantity`,`price` FROM products WHERE id_product= :id_product";
+    $sql = "SELECT `id_product`,name,`quantity`,`price` FROM products WHERE id_product= :id_product";
     //$sql = "SELECT `nom`,`age` FROM noms WHERE id_nom=11";
     $stmt = $bdd->prepare($sql);
     $stmt->bindValue(":id_product",$id_product,PDO::PARAM_INT);
@@ -32,6 +32,7 @@ $response = [
         $data = $stmt->fetch();
         
             $json["id_product"]= $data["id_product"];
+            $json["name"]= $data["name"];
             $json["quantity"]= $data["quantity"];
             $json["price"]= $data["price"];
        
