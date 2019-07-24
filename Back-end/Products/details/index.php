9e60c706720e3e52d2ba9c7e865e46f4180bb0de
zaidun <zaidun10@yahoo.com>
2019-07-24 10:16:18 +0200

    afficher les detaisl

--- Back-end/Products/details/index.php
+++ Back-end/Products/details/index.php
@@ -17,28 +17,32 @@ $response = [
     "error_message" => "Uknown Error",
     "products"          => NULL
 ];
-
-
-$sql = "SELECT `id_products`,`quantity`,`price` FROM products;";
-$stmt = $bdd->prepare($sql);
-$result = $stmt->execute();
-
-if($result)
+if(isset($_REQUEST['id_products']))
 {
-    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    $response["products"] = $data;
-    $response["error_message"] = "";
-    $response["error"] = false;
+    $id_product = $_REQUEST['id_products'];
+    
+    $sql = "SELECT `id_products`,`quantity`,`price` FROM products WHERE id_products= :id_products";
+    $stmt = $bdd->prepare($sql);
+    $stmt->bindValue(":id_products",$id_product,PDO::PARAM_INT);
+    $result = $stmt->execute();
+
+    if($result)
+    {
+        $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $response["products"] = $data;
+        $response["error_message"] = "";
+        $response["error"] = false;
+    }
+    else
+    {
+        $response["error_message"] = "ERROR QUERY";
+    }
+
+
+    $stmt->closeCursor();
+    
+
+    echo json_encode($response);
+
+    die();
 }
-else
-{
-    $response["error_message"] = "ERROR QUERY";
-}
-
-
-$stmt->closeCursor();
-   
-
-echo json_encode($response);
-
-die();
