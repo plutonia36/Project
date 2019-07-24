24b6d1ba188b2d974224a660b88c081a3b975e22
plutonia36 <53211532+plutonia36@users.noreply.github.com>
2019-07-24 15:29:59 +0200

    fin

--- Back-end/Products/add/index.php
+++ Back-end/Products/add/index.php
@@ -8,37 +8,45 @@ require "../../libs/connexiondb.php";
 header('Content-Type: application/json');
 
 $response = [
-    "error"         => true,
-    "error_message" => "Unknow error",
-    "products"          => NULL,
-    "status"            => ""
+    "error"             => true,
+    "error_message"     => "Erreur parametres",
+    //"products"          => NULL,
+    "status"            => "non"
 ];
-    if(isset($_GET["name"], $_GET["quantity"], $_GET["price"]))
+    if(isset($_REQUEST["name"], $_REQUEST["quantity"], $_REQUEST["price"]))
     {
 
-        $name=$_GET["name"];
-        $quantity=$_GET["quantity"];
-        $price=$_GET["price"];
+        $name=$_REQUEST["name"];
+        $quantity=$_REQUEST["quantity"];
+        $price=$_REQUEST["price"];
 
 
         $sql="INSERT INTO products (name,quantity,price) VALUES (:name,:quantity,:price);";
+
         $stmtnt = $bdd->prepare($sql);
         $stmtnt->bindValue(":name", $name, PDO::PARAM_STR);
         $stmtnt->bindValue(":quantity", $quantity, PDO::PARAM_INT);
         $stmtnt->bindValue(":price", $price, PDO::PARAM_INT);
         $stmtnt->execute();
+
         if($stmtnt)
-    {   
-        $data = "ok";
-        $response["products"] = $data;
-        $response["error_message"] = "";
-        $response["status"] = "bon";
-
-     die();
-
-    }else{
-        $reponse["error_message"] = "mmettre le nom le poids et le prix dans l url";
-    }
-        echo json_encode($response);
-
-    }
+        {   
+        
+            //$data = "ok";
+            $response["error"] = false;
+            $response["error_message"] = "";
+            $response["status"] = "ok";
+         }else
+         {
+            $response["error"] = true;
+            $response["error_message"] = "erreur requete sql";
+            $response["status"] = "non";
+         }
+         echo json_encode($response);
+}else
+{
+            $response["error"] = true;
+            $response["error_message"] = "pas de parametre";
+            $response["status"] = "no";
+            echo json_encode($response); 
+}
--- Back-end/Products/modify/index.php
+++ Back-end/Products/modify/index.php
@@ -12,33 +12,22 @@ $reponse = [
     "data"          => "" 
 ];
 
-if(isset($_GET["name"],$_GET["quantity"],$_GET["price"]))
+if(isset($_GET["name"],$_GET["quantity"],$_GET["price"], $_GET["id_product"]))
 {
     $name=$_GET["name"];
     $quantity=$_GET["quantity"];
     $price=$_GET["price"];
+    $id_product=$_GET["id_product"];
     
-    $sql = "UPDATE products SET name =$name, quantity=$quantity, price=$price WHERE name=:name AND quantity=:quantity AND price=:price;";
+    $sql = "UPDATE products SET name =$name, quantity=$quantity, price=$price 
+    WHERE name=:name AND quantity=:quantity AND price=:price AND id_product=:id_product;";
     $stmtnt = $bdd->prepare($sql);
     $stmtnt->bindValue("name",$name,PDO::PARAM_STR);
     $stmtnt->bindValue("quantity",$quantity,PDO::PARAM_INT);
     $stmtnt->bindValue("price",$price,PDO::PARAM_INT);
+    $stmtnt->bindValue("id_product",$id_product,PDO::PARAM_INT);
     $stmtnt->execute();
-    if($stmtnt)
-    {
-        $data = "ok";
-        $response["products"] = $data;
-        $response["error_message"] = "";
-        $response["error"] = false;
-    }
-    else
-    {
-        $response["error_message"] = "Erreur parametre: fonction 'name','quantity' et 'price' manquante ";
-    }
     
-    echo json_encode($reponse);
-    
-    die();
 }
-
+echo json_encode($reponse);
 
