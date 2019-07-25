e3bc394316bf72b6f6e51e750892ce3c235c4c57
plutonia36 <53211532+plutonia36@users.noreply.github.com>
2019-07-25 14:48:10 +0200

    modif

--- Back-end/Products/modify/index.php
+++ Back-end/Products/modify/index.php
@@ -20,12 +20,13 @@ if(isset($_REQUEST["name"],$_REQUEST["quantity"],$_REQUEST["price"], $_REQUEST["
     $id_product=$_REQUEST["id_product"];
     
     //$sql = "UPDATE products SET name =:name, quantity=$quantity, price=$price WHERE id_product=:id_products;";
-    $sql = "UPDATE products SET name = :name, quantity= :quantity, price= :price WHERE id_product=:id_products;";
+    $sql = "UPDATE products SET name = :name, quantity= :quantity, price= :price WHERE id_product=:id_product;";
 
     $stmtnt = $bdd->prepare($sql);
-    $stmtnt->bindValue("name",$name,PDO::PARAM_STR);
-    $stmtnt->bindValue("quantity",$quantity,PDO::PARAM_INT);
-    $stmtnt->bindValue("price",$price,PDO::PARAM_INT);
+    $stmtnt->bindValue(":name",$name,PDO::PARAM_STR);
+    $stmtnt->bindValue(":quantity",$quantity,PDO::PARAM_INT);
+    $stmtnt->bindValue(":price",$price,PDO::PARAM_INT);
+    $stmtnt->bindValue(":id_product", $id_product, PDO::PARAM_INT);
     $stmtnt->execute();
 
     if($stmtnt){
